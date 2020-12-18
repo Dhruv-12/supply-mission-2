0cbd3d615a09b65c8a98a22c220025b8ed385e62
Dhruv-12 <74650148+Dhruv-12@users.noreply.github.com>
2020-12-18 21:01:34 +0530

    Add files via upload

---
+++ staticBodies.js
@@ -0,0 +1,24 @@
+class StaticBodies{
+    constructor(x,y,width,height){
+    var options={
+        isStatic :true
+    }
+
+    this.body=Bodies.rectangle(x,y,width,height,options);
+    World.add(world,this.body);
+    this.width=width;
+    this.height=height;
+    
+}
+
+display(){
+    rectMode(CENTER);
+    var pos=this.body.position;
+    rect(pos.x,pos.y, this.width, this.height);
+
+
+
+
+}
+
+}
